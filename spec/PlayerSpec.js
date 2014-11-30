5e071b0d296fa42877156f4950f45d15bd9320d5
danielobembe <ayo.obembe@gmail.com>
2014-11-30 16:11:22 +0000

    Added more functionality to player and associated changes made in Frame

--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -5,12 +5,6 @@ describe('Player',function() {
 		expect(player.hasOwnProperty('frame')).toBe(true);
 	});
 
-
-	it('Should have 2 tries(or rolls) when initialized', function() {
-		var player = new Player();
-		expect(player.tries).toEqual(2);
-	});
-
 	it('Roll should modifiy the pin count in its frame', function() {
 		var player = new Player();
 		expect(player.frameCount()).toEqual(10);
@@ -18,16 +12,36 @@ describe('Player',function() {
 		expect(player.frameCount()).toEqual(7);
 	});
 
+	it('Should have 2 tries(or rolls) when initialized', function() {
+		var player = new Player();
+		expect(player.tries).toEqual(2);
+	});
+
 	it('Roll should modify the number of tries left', function() {
-		
+		var player = new Player();
+		expect(player.tries).toEqual(2);
+		player.roll(1);
+		expect(player.tries).toEqual(1);
+		player.roll(2);
+		expect(player.tries).toEqual(0);
 	});
 
-	it('should have a score of 0 when initialized',function(){ 
-
+	it('Should have a score of 0 when initialized',function(){ 
+		var player = new Player();
+		expect(player.score).toEqual(0);
 	});
+
+	it('Should be able to update its score', function() {
+		var player = new Player();
+		player.updateScore(4);
+		expect(player.score).toEqual(4);
+	}); 
 
 	it('should update its score on each roll', function() {
-
+		var player = new Player();
+		var luckyShot = 6;
+		player.roll(luckyShot);
+		expect(player.score).toEqual(luckyShot);
 	});
 
 
--- src/Player.js
+++ src/Player.js
@@ -1,6 +1,7 @@
 function Player() {
 	this.frame = new Frame();
 	this.tries = 2;
+	this.score = 0;
 };
 
 Player.prototype.frameCount = function() {
@@ -9,4 +10,10 @@ Player.prototype.frameCount = function() {
 
 Player.prototype.roll = function(number){
 	this.frame.elim(number);
+	this.tries = this.tries - 1;
+	this.updateScore(number);
+};
+
+Player.prototype.updateScore = function(number){
+	this.score = this.score + number;
 };
