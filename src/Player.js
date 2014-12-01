2c1a49d5a16a1d81d287fbcddfa8d2b45010d06a
danielobembe <ayo.obembe@gmail.com>
2014-12-01 04:47:38 +0000

    modified player and spec some more, can't remember what changes but still working

--- src/Player.js
+++ src/Player.js
@@ -2,28 +2,28 @@ function Player() {
 	this.frame = new Frame();
 	this.tries = 2;
 	this.score = 0;
+	this.shot = 0;
 };
 
+//Number of pins in the frame
 Player.prototype.frameCount = function() {
 	return this.frame.count();
 };
 
+//A roll reduces number of pins in the frame
 Player.prototype.roll = function(number){
 	this.frame.elim(number);
 	this.tries = this.tries - 1;
 	this.updateScore(number);
 };
 
+//keeps count of the player's score
 Player.prototype.updateScore = function(number){
 	this.score = this.score + number;
 };
 
 
-
-
-//Integrate randomGen or rand into code
-
-
+//These functions are used to generate random scores
 function randomGen(){
 	return Math.floor(Math.random()*11);
 }
